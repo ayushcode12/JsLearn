2af571cdbc94716ef852d867c3a7245e7d87f4f2
ayushcode12 <116939675+ayushcode12@users.noreply.github.com>
2025-06-24 06:36:18 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 145553
+let accountEmail = "Ayush@gmail"
+var accoutPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 //not allowed
+
+accountEmail = "gdugufvugys"
+accoutPassword = "987654"
+accountCity = "bangaluru"
+
+console.log(accountId)
+ 
+console.table([accountEmail, accoutPassword, accountCity])
