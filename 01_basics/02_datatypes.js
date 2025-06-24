5a988d5e6b110d4cd36eb112ac0d007b7c351cff
ayushcode12 <116939675+ayushcode12@users.noreply.github.com>
2025-06-24 06:36:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+let name = "Ayush"
+let age = 21
+let isLoggedIn = true
+
+// number ranges 2^53
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+
+// object
+
+console.log(typeof"Ayush")
+console.log(typeof 21)
+console.log(typeof true)
